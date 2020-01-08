2371f8e84dbf6136235348618a448c96d3ccfff3
JennyFive <wzn0408@163.com>
2020-01-08 22:40:01 +0800

    更新排序

--- python数据结构与算法分析/排序/bubbleSort.py
+++ python数据结构与算法分析/排序/bubbleSort.py
@@ -10,3 +10,73 @@ def bubbleSort(alist):
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
+
+def shortBubbleSort(alist):
+    exchange = True
+    passnum = len(alist) - 1
+
+    while passnum > 0 and exchange:
+        exchange = False
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                exchange = True
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        passnum -= 1
+
+"""
+选择排序。每次遍历只交换一次。
+O(n^2)
+"""
+def selectionSort(alist):
+    for fillslot in range(len(alist)-1, 0, -1):
+        positionMax = 0
+        for location in range(1, fillslot):
+            if alist[location] > alist[positionMax]:
+                positionMax = location
+        
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionMax]
+        alist[positionMax] = temp
+
+"""
+插入排序。
+O(n^2)
+"""
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+
+        currentvalue = alist[index]
+        position = index
+
+        while position > 0 and alist[position-1] > currentvalue:
+            alist[position] = alist[position-1]
+            position -= 1
+
+        alist[position] = currentvalue
+
+"""
+希尔排序。
+O(n)~O(n^2)
+"""
+def shellSort(alist):
+    sublistcount = len(alist) // 2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+
+        print("After increments of size", sublistcount, "the list ia", alist)
+
+        sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start+gap, len(alist), gap):
+        currentvalue = alist[i]
+        position = i
+
+        while position > gap and alist[position-gap] > currentvalue:
+            alist[position] = alist[position-gap]
+            position = position - gap
+
+        alist[position] = currentvalue
